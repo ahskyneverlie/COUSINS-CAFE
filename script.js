4e609cf597127460271425b1a84807d9fadf3f97
ahskyneverlie <1hitfromsky@gmail.com>
2025-03-03 17:03:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// script.js
-function moveToGroup(name, type) {
-    const coffeeGroup = document.getElementById('coffee-group-items');
-    const milkTeaGroup = document.getElementById('milk-tea-group-items');
 
-    // 创建新的列表项
-    const li = document.createElement('li');
-    li.textContent = name;
-
-    // 根据类型分配到不同的组
-    if (type === '咖啡') {
-        coffeeGroup.appendChild(li);
-    } else if (type === '奶茶') {
-        milkTeaGroup.appendChild(li);
-    }
-}
